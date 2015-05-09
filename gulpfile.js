03f898d580f6e870978c9408500307f4658e8c43
medmunds <medmunds@gmail.com>
2015-05-08 18:56:06 -0700

    gulp clean, install

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,101 @@
 var child_process = require('child_process');
 var escapeRegExp = require('lodash.escaperegexp');
+var expandHomeDir = require('expand-home-dir');
+var fs = require('fs');
 var gulp = require('gulp');
 var identity = require('lodash.identity');
 var mkdirp = require('mkdirp');
+var ncp = require('ncp');
+var os = require('os');
 var packager = require('electron-packager');
 var path = require('path');
+var rimraf = require('rimraf');
 
-var electronVersion = require('electron-prebuilt/package.json').version;
-var projectVersion = require('./package.json').version;
-var projectName = require('./package.json').name;
-var productName = require('./src/config.js').appName;
+var platform = os.platform();
+
+//var electronVersion = require('electron-prebuilt/package.json').version;
+var appName = require('./src/config.js').appName;
+var appVersion = require('./package.json').version;
+
 
 var srcDir = '.'; // everything from here will get copied into the app (filtered by ignore and prune below)
-var distDir = './dist';
+var buildDir = './build';
+
+var installDir = {
+    'darwin': expandHomeDir('~/Applications'),
+    'linux': expandHomeDir('~/bin')
+}[platform];
+
+// Ugh. Needs to match electron-packager output:
+var executableName = {
+    'darwin': appName + '.app',
+    'linux': appName,
+    'windows': appName + '.exe'
+}[platform] || '';
 
 
-gulp.task('distdirs', function(cb) {
-    mkdirp(distDir, cb);
+gulp.task('default', ['build']);
+
+
+gulp.task('clean', function(cb) {
+    rimraf(buildDir, cb);
+});
+
+
+gulp.task('build', ['clean'], function(cb) {
+    var ignored = getIgnored();
+    mkdirp(buildDir, function(err) {
+        if (err) return cb(err);
+        packager({
+            dir: '.', // source dir
+            out: buildDir, // dest dir -- must exist
+            name: appName,
+            ignore: ignored, // array of regexps for sources to exclude -- *not* cwd-relative
+            prune: true, // npm prune devDependencies out of app
+            asar: false, // bundles app source into an asar
+
+            // Mac-specific packaging:
+            //icon: "./src/assets/app.icns",
+            //'app-bundle-id': "com.example.myapp", // default 'com.electron.' + opts.name.toLowerCase()
+            //'helper-bundle-id': "com.example.myapp.helper",  // default 'com.electron.' + opts.name.toLowerCase() + '.helper'
+            'app-version': appVersion // default none
+            //protocols: [{ schemes: ["scheme"], name: "name" }, ... ],
+        }, function(err, finalPath) {
+            if (err) return cb(err);
+            console.log("Completed building " + finalPath);
+            return cb(null, finalPath);
+        });
+    });
+});
+
+
+gulp.task('install', function(cb) {
+    if (!installDir) {
+        return cb(new Error("Don't know how to install on platform " + platform));
+    }
+
+    var builtApp = path.join(buildDir, executableName),
+        installedApp = path.join(installDir, executableName);
+    // Make sure it's built before we delete the old one...
+    fs.access(builtApp, fs.R_OK, function(err) {
+        if (err) return cb(err);
+        rimraf(installedApp, function(err) {
+            if (err) return cb(err);
+            ncp(builtApp, installedApp, function(err) {
+                if (err) return cb(err);
+                console.log("Installed to " + installedApp);
+                return cb(null, installedApp);
+            });
+        });
+    });
 });
 
 
 gulp.task('list-ignored', function() {
+    // helpful for debugging ignore list
     console.log(getIgnored());
 });
 
-
-gulp.task('build', ['distdirs'], function(cb) {
-    var ignored = getIgnored();
-    packager({
-        dir: '.', // source dir
-        out: distDir, // dest dir -- must exist
-        name: productName,
-        ignore: ignored, // array of regexps for sources to exclude -- *not* cwd-relative
-        prune: true, // npm prune devDependencies out of app
-        asar: false, // bundles app source into an asar
-
-        // Mac-specific packaging:
-        //icon: "./src/assets/app.icns",
-        //'app-bundle-id': "com.example.myapp", // default 'com.electron.' + opts.name.toLowerCase()
-        //'helper-bundle-id': "com.example.myapp.helper",  // default 'com.electron.' + opts.name.toLowerCase() + '.helper'
-        'app-version': projectVersion // default none
-        //protocols: [{ schemes: ["scheme"], name: "name" }, ... ],
-    }, cb);
-});
 
 
 function getIgnored() {
@@ -62,7 +115,7 @@ function getIgnored() {
         }).filter(identity);
 
     // ignore our own built apps:
-    ignored.push(makeIgnoreRegExp(distDir));
+    ignored.push(makeIgnoreRegExp(buildDir));
 
     // ignore .git and .gitignore anywhere in the tree:
     ignored.push(/(^|\/)\.git$/);
